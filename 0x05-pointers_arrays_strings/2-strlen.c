2f90dd04f75e35ee23494d8c49203eabb030573f
Sambo Saidu <sambosaidu887@gmail.com>
2022-10-06 09:01:22 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+/**
+ * _strlen - counts the total length of passed
+ *		argument
+ *
+ * @s: char pointer
+ *
+ * Return: len
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(*s)
+
+	return (len);
+
+}
