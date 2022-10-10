b202664ae852f91840faf91e114e26e14f05a97a
Sambo Saidu <sambosaidu887@gmail.com>
2022-10-10 09:58:39 -0700

    init all

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,18 +2,28 @@
 #define DOG_H
 
 /**
- * struct dog - class dog with variables
- * @name: char *
- * @age: float
- * @owner: char *
+ * struct dog - dog structure
  *
- * Description: - No log description
- */
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: dog owner
+ *
+*/
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t -Typedef for struct dog
+*/
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* struct dog */
