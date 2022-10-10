8a30af71d12829084ce99878128d692d7b91ec2e
Sambo Saidu <sambosaidu887@gmail.com>
2022-10-10 11:03:05 -0700

    repaired 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -3,9 +3,4 @@
 
 #define ABS(x) (((x) >= 0) ? (x) : -(x))
 
-#endif /* _FUNCTION_LIKE_MACRO_H */#ifndef _FUNCTION_LIKE_MACRO_H
-#define _FUNCTION_LIKE_MACRO_H
-
-#define ABS(x) (((x) >= 0) ? (x) : -(x))
-
 #endif /* _FUNCTION_LIKE_MACRO_H */
