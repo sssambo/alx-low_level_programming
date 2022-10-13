ed97be46710e753a4bf9098fac87adf55802c8af
Sambo Saidu <sambosaidu887@gmail.com>
2022-10-13 08:07:53 -0700

    first push

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+/**
+ * sum_them_all - function to print all the specified arguments
+ * @n: int specifier
+ *
+ * Return: accum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list list;
+	int counter;
+	int accum;
+
+	if (n == 0)
+	return (0);
+
+	counter = n;
+	accum = 0;
+	va_start(list, n);
+	while (count != 0)
+	{
+		accum += va_arg(list, int);
+		count--;
+	}
+	va_end(list);
+	return (accum);
+}
