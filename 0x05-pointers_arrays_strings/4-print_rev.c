9c0c292512600320c3589a0b00041163d6803018
Sambo Saidu <sambosaidu887@gmail.com>
2022-10-15 10:33:08 -0700

    number 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - reverse string in stdout
+ * @s: char pointer
+ *
+ * Return: void through
+ */
+void print_rev(char *s)
+{
+        int size = 0;
+
+        while (s[size] != '\0')
+            size++;
+        while(size >= 0)
+        {
+            putc(s[size], stdout);
+            size--;
+        }
+}
